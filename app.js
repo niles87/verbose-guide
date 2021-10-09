fe728d4488637f3a94d881be5585fea876f6001b
Niles Bingham <nbing19@gmail.com>
2021-10-08 20:13:40 -0700

    added start script and fixed app engine

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const multer = require('multer');
 const exhbs = require('express-handlebars');
-const path = require('path')
+const path = require('path');
+const upload = multer({dest: 'uploads/'});
 
 const app = express();
 
-app.engine('handlebars', exhbs.engine);
+app.engine('handlebars', exhbs());
 app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
