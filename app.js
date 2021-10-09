3b0c994ec4715901ab42477c6a75437c0a403001
Niles Bingham <nbing19@gmail.com>
2021-10-08 19:25:10 -0700

    basic server file

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const multer = require('multer');
+const exhbs = require('express-handlebars');
+const path = require('path')
+
+const app = express();
+
+app.engine('handlebars', exhbs.engine);
+app.set('view engine', 'handlebars');
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.listen(3001, () => {
+    console.log('express is listening http://localhost:3001');
+});
