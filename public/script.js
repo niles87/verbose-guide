2e0af8860fe534a1b250e0f3b692375063b005ea
Niles Bingham <nbing19@gmail.com>
2021-10-09 18:35:34 -0700

    updated public code to match markup

--- public/script.js
+++ public/script.js
@@ -1,16 +1,34 @@
-window.addEventListener('load', function() {
-
-    form.addEventListener('submit', function(e) {
-        e.preventDefault();
-        const formData = new FormData(form)
-        for (let [name, value] of formData.entries()) {
-            console.log(name, value)
-        }
-        fetch('/file-upload',{
-            method: 'post',
-            body: formData
-        }).then(function(){
-            console.log('complete')
-        })
+window.addEventListener("load", function () {
+  multerForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const formData = new FormData(multerForm);
+    for (let [name, value] of formData.entries()) {
+      console.log(name, value);
+    }
+    fetch("/file-upload", {
+      method: "post",
+      body: formData,
+    }).then(function () {
+      console.log("complete");
     });
-})
+  });
+
+  withoutMulter.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const formData = new FormData(withoutMulter);
+    for (let [name, value] of formData.entries()) {
+      console.log(name, value);
+    }
+  });
+
+  dropZone.addEventListener("dragover", function (e) {
+    e.preventDefault();
+    console.log("dragging");
+    dropZone.firstElementChild.textContent = "Drop file here..";
+  });
+
+  dropZone.addEventListener("drop", function (e) {
+    e.preventDefault();
+    console.log("dropped");
+  });
+});
