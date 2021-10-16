4769bbac52e23acbf793f134238ce70435c9cc69
Niles Bingham <nbing19@gmail.com>
2021-10-16 08:59:21 -0700

    added frontend javascript and adjusted styles

--- public/script.js
+++ public/script.js
@@ -19,6 +19,16 @@ window.addEventListener("load", function () {
     for (let [name, value] of formData.entries()) {
       console.log(name, value);
     }
+    fetch("/no-multer", {
+      method: "post",
+      body: formData,
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        console.log(json);
+      });
   });
 
   dropZone.addEventListener("dragover", function (e) {
@@ -30,5 +40,6 @@ window.addEventListener("load", function () {
   dropZone.addEventListener("drop", function (e) {
     e.preventDefault();
     console.log("dropped");
+    console.log(e);
   });
 });
